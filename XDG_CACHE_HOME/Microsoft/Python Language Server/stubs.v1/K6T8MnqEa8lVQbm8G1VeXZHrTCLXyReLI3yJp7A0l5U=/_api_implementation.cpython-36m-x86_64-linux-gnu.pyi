51c152150b0f7a1bb17db4eb64c26da6d55981a1
Lab <ennisnie@tongji.edu.cn>
2019-06-08 22:30:14 +0800

    aaa

--- XDG_CACHE_HOME/Microsoft/Python Language Server/stubs.v1/K6T8MnqEa8lVQbm8G1VeXZHrTCLXyReLI3yJp7A0l5U=/_api_implementation.cpython-36m-x86_64-linux-gnu.pyi
+++
@@ -1,5 +0,0 @@
-__doc__ = '_api_implementation is a module that exposes compile-time constants that\ndetermine the default API implementation to use for Python proto2.\n\nIt complements api_implementation.py by setting defaults using compile-time\nconstants defined in C, such that one can set defaults at compilation\n(e.g. with blaze flag --copt=-DPYTHON_PROTO2_CPP_IMPL_V2).'
-__file__ = '/home/nj/.conda/envs/cs231n/lib/python3.6/site-packages/google/protobuf/internal/_api_implementation.cpython-36m-x86_64-linux-gnu.so'
-__name__ = 'protobuf.internal._api_implementation'
-__package__ = 'protobuf.internal'
-api_version = 2
--- XDG_CACHE_HOME/Microsoft/Python Language Server/stubs.v1/g-4MDQP4Vyi_PKSi6dD25psRbb75FS8gxee6efJKj5o=/_mklinit.cpython-36m-x86_64-linux-gnu.pyi
+++
@@ -1,4 +0,0 @@
-__doc__ = None
-__file__ = '/home/nj/.conda/envs/cs231n/lib/python3.6/site-packages/numpy/_mklinit.cpython-36m-x86_64-linux-gnu.so'
-__name__ = 'mklinit'
-__package__ = 'numpy'
